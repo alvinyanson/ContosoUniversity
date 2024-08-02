fa8ad45c89a8f5b58641f660b785efe507a4284f
alvinyanson <a.yanson@arcanys.com>
2024-08-02 13:45:08 +0800

    feat: create complex data model

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,6 +1,8 @@
-﻿namespace ContosoUniversity.Models
-{
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace ContosoUniversity.Models
+{
     public enum Grade
     {
         A, B, C, D, F
@@ -11,6 +13,7 @@
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
